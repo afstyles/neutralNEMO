2947a5e69fb31d6c91140a9ba352d4010aaeb1f0
Andrew Styles <andrew.styles@linacre.ox.ac.uk>
2024-10-22 16:20:38 +0100

    Added calculation of deptht if not present in grid file. Updated commenting

--- src/neutralNEMO/grid.py
+++ src/neutralNEMO/grid.py
@@ -197,7 +197,7 @@ def load_zgriddata( path, deptht_varname="gdept_0", tmask3d_varname="tmask", tma
                     infer_tmask2d=False, blev_varname="bottom_level", infer_tmask3d=False, 
                     infer_path='nemo-T.nc', infer_varname='so', 
                     infer_tdim="time_counter", infer_idim="x", infer_jdim="y", infer_zdim="deptht",
-                    infer_val=None, **kwargs ):
+                    infer_val=None, calc_deptht=False, e3w_varname='e3w_0', **kwargs ):
     """
     Create zgriddata dictionary by loading from a standard NEMO output (either a 
     mesh_mask or domaincfg file) or a netcdf file containing the necessary C-grid
@@ -240,11 +240,77 @@ def load_zgriddata( path, deptht_varname="gdept_0", tmask3d_varname="tmask", tma
         Dimension name for the j axis. 
         Can be found using `ncdump -h your_file.nc`
 
+    calc_deptht: Logical (optional, default = False)
+        calc_deptht = False -> Load deptht from the file using the variable name
+                               defined by deptht_varname
+        calc_deptht = True -> Calculate deptht using cell thicknesses in the file
+
+    e3w_varname: String (optional, default = 'e3w_0')
+        Only used if calc_deptht == True
+        Variable name for W cell thickness in the file.
+
+    infer_tmask2d: Logical (optional, default = False)
+        infer_tmask2d = False -> Load the two-dimensional T mask from the file
+                                 using the variable name defined by tmask2d_varname
+                        True  -> Deduce the two-dimensional T mask from the bottom 
+                                 level variable in the file. The bottom level variable
+                                 name is defined by blev_varname
+
+    blev_varname: String (optional, default = 'bottom_level')
+        Only used if infer_tmask2d == True
+        Variable name for bottom level in the file
+
+    infer_tmask3d: Logical (optional, default = False)
+        infer_tmask3d = False -> Load the three-dimensional T mask from the file
+                                 using the variable name defined by tmask3d_varname
+                        True  -> Deduced the three-dimensional T mask from a file
+                                 defined by infer_path (infer_path can equal path), using
+                                 the variable name defined by infer_varname. The dimension
+                                 names for the variable are defined by infer_tdim, infer_idim,
+                                 infer_jdim, infer_zdim (details below). The method for deducing
+                                 the mask depends on the argument infer_val (see below).
+
+    infer_val: String or Float (optional, default = None)
+        Only used if infer_tmask3d == True
+        infer_val = None -> The three-dimensional T mask is deduced from the infer variable by
+                            identifying null values (e.g. Nan).
+                  = Float -> The three-dimensional T mask is deduced from the infer variable by
+                             finding occurences of infer_val. Where the infer variable == infer_val,
+                             tmask3d = False.
+
+    infer_path: String (optional, default = "nemo-T.nc")
+        Only used if infer_tmask3d == True
+        Location of the file to read when deducing the three-dimensional T mask.
+
+    infer_varname: String (optional, default = "so")
+        Only used if infer_tmask3d == True
+        Name of the variable to use when deducing the three-dimensional T mask.
+
+    infer_tdim: String (optional, default = "time_counter")
+        Only used if infer_tmask3d == True
+        Dimension name for the time axis in the infer variable file (defined by infer_path)
+        Can be found using `ncdump -h your_file.nc`
+
+    infer_idim: String (optional, default = "x")
+        Only used if infer_tmask3d == True
+        Dimension name for the i axis in the infer variable file (defined by infer_path)
+        Can be found using `ncdump -h your_file.nc`
+
+    infer_jdim: String (optional, default = "y")
+        Only used if infer_tmask3d == True
+        Dimension name for the i axis in the infer variable file (defined by infer_path)
+        Can be found using `ncdump -h your_file.nc`
+
+    infer_zdim: String (optional, default = "deptht")
+        Only used if infer_tmask3d == True
+        Dimension name for the z axis in the infer variable file (defined by infer_path)
+        Can be found using `ncdump -h your_file.nc`                          
+
     open_mf: Logical (optional, default = False)
         open_mf = False -> Load a dataset from a single netcdf file 
                            (see xarray.open_dataset)
         open_mf = True -> Load a dataset from multiple netcdf files 
-                          (see xarray.open_mfdataset)
+                          (see xarray.open_mfdataset)        
 
     kwargs: Keyword arguments
         Keyword arguments for xarray.open_dataset (default, open_mf=False) 
@@ -292,10 +358,16 @@ def load_zgriddata( path, deptht_varname="gdept_0", tmask3d_varname="tmask", tma
     
     zgriddata = {}
 
-    try:
-        zgriddata["deptht"] = dataset[deptht_varname].squeeze()
-    except:
-        print("Variable {} not found in: {}".format(deptht_varname, path))
+    if calc_deptht == False:
+        try:
+            zgriddata["deptht"] = dataset[deptht_varname].squeeze()
+        except:
+            print("Variable {} not found in: {}".format(deptht_varname, path))
+            print("Try running with calc_deptht = True")
+            print('Or add the depth manually using the key "deptht"')
+    else:
+        e3w = dataset[e3w_varname]
+        zgriddata["deptht"] = e3w.cumsum(dim='z_t').squeeze()
 
     if infer_tmask3d == False:
         print("Loading masks directly from files")
@@ -303,6 +375,8 @@ def load_zgriddata( path, deptht_varname="gdept_0", tmask3d_varname="tmask", tma
             zgriddata["tmask3d"] = dataset[tmask3d_varname].squeeze().astype(bool)
         except:
             print("Variable {} not found in: {}".format(tmask3d_varname, path))
+            print("Try running with infer_tmask3d = True")
+            print('Or add the mask manually using the key "tmask3d"')
 
     else:
         print("Inferring tmask3d from other data field [Variable {} in File {}]".format(infer_varname, infer_path))
@@ -315,16 +389,19 @@ def load_zgriddata( path, deptht_varname="gdept_0", tmask3d_varname="tmask", tma
             zgriddata["tmask3d"] = xr.where(infer_var == infer_val, False, True).astype(bool)
 
     if infer_tmask2d == False:
-
         try:
             zgriddata["tmask2d"] = dataset[tmask2d_varname].squeeze().astype(bool)
         except:
             print("Variable {} not found in: {}".format(tmask2d_varname, path))
+            print("Try running with infer_tmask2d = True")
+            print('Or add the tmask2d manually using the key "tmask2d"')
 
     else:
         print("Inferring tmask2d from bottom level data [Variable {} in file {}]".format(blev_varname, path))
         blev = dataset[blev_varname].squeeze().astype(int)
         zgriddata["tmask2d"] = xr.where(blev == 0, False, True).astype(bool)
+
+    
 
     return zgriddata
 
